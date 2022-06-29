af1a040a4ecf1c99bb1b0d6b71b7bcea575b9c71
daylin2022 <daylin.murga@gizlocorp.com>
2022-06-29 10:06:53 -0500

    modulos

--- l10n_ec_check_printing/models/account_payment.py
+++ l10n_ec_check_printing/models/account_payment.py
@@ -512,7 +512,7 @@ class AccountPayment(models.Model):
                 monto_pendiente_pago=0
                 obj_factura=self.env['account.move'].browse(factura)
                 for x in obj_factura.contrato_estado_cuenta_ids:
-                    monto_pendiente_pago+=(x.saldo_cuota_capital+x.saldo_seguro+x.saldo_rastreo+x.saldo_otros)
+                    #monto_pendiente_pago+=(x.saldo_cuota_capital+x.saldo_seguro+x.saldo_rastreo+x.saldo_otros)
                 line_id = PaymentLine.create([{
                     'invoice_id': obj_factura.id,
                     'amount_total': obj_factura.amount_total,
