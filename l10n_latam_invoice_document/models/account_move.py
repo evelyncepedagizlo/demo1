5a0f927217c70f990353ebdb530602acf2f6b5dd
daylin2022 <daylin.murga@gizlocorp.com>
2022-06-29 12:41:20 -0500

    modulos

--- l10n_latam_invoice_document/models/account_move.py
+++ l10n_latam_invoice_document/models/account_move.py
@@ -149,6 +149,7 @@ class AccountMove(models.Model):
 
     @api.depends('journal_id', 'partner_id', 'company_id', 'type')
     def _compute_l10n_latam_available_document_types(self):
+        raise ValidationError('{0}'.format(self.l10n_latam_use_documents))
         self.l10n_latam_available_document_type_ids = False
         for rec in self.filtered(lambda x: x.journal_id and x.l10n_latam_use_documents and x.partner_id):
             rec.l10n_latam_available_document_type_ids = self.env['l10n_latam.document.type'].search(rec._get_l10n_latam_documents_domain())
